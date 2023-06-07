83fa3f224a610006d07ffda7f6e4b267e029cb83
unknown <mndprojects>
2023-06-07 19:26:11 +0200

    init

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/test', async function (req, res) {
+    try {
+        const dateString = req.query.date;
+        const epochMilliseconds = convertToEpoch(dateString);
+        res.send({ result: epochMilliseconds })
+    } catch (er) {
+        res.send({ error: er })
+    }
 
-    res.send({page:"test ya 5ara"})
 })
 
 app.get('/', async function (req, res) {
 
-    res.send({page:"main"})
+    res.send({ page: "main" })
 })
 
-
+function convertToEpoch(dateString) {
+    const [month, day, year] = dateString.split('.');
+    const dateObject = new Date(`${year}-${month}-${day}`);
+    const milliseconds = dateObject.getTime();
+    return milliseconds;
+}
 
 var server = app.listen(port, function () {
     var host = server.address().address;
